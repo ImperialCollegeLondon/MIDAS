af362e1c4f0b37f573022e0507703596340e7a11
John Tate <jt6@sanger.ac.uk>
2015-05-08 14:47:49 +0100

    Add tests for audit logging

---
+++ app/t/etc/audit_logging.pl
@@ -0,0 +1,15 @@
+# database definition used by t/audit_logging.t
+{
+  schema_class => 'Bio::HICF::Schema',
+  resultsets => [ qw(
+    User
+  ) ],
+  fixture_sets => {
+    main => [
+      User => [
+        [ qw( username passphrase displayname email roles api_key ) ],
+        [ 'testuser', 'password', 'Test User', 'testuser@sanger.ac.uk', 'user', '2566ZD3k4SVdJfGkdXJQUj6B4aPoq2Rf' ],
+      ],
+    ],
+  },
+};
