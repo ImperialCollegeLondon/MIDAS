297b1ac8c47dd161ebc4b034a223f40b34fa8898
John Tate <jt6@sanger.ac.uk>
2015-06-16 16:49:23 +0100

    Add method to serialize data to CSV

--- app/lib/MIDAS/Base/Controller/Restful.pm
+++ app/lib/MIDAS/Base/Controller/Restful.pm
@@ -4,6 +4,8 @@ package MIDAS::Base::Controller::Restful;
 use Moose;
 use namespace::autoclean;
 
+use Text::CSV_XS;
+
 BEGIN { extends 'Catalyst::Controller::REST' }
 
 #-------------------------------------------------------------------------------
@@ -16,8 +18,57 @@ __PACKAGE__->config(
     'text/yaml'          => 'YAML',
     'text/x-yaml'        => 'YAML',
     'application/x-yaml' => 'YAML',
+    'text/csv'           => [ 'Callback', { serialize => \&_render_csv } ],
   }
 );
+
+#-------------------------------------------------------------------------------
+
+# an instance of Text::CSV_XS, used for rendering CSV output
+has '_csv' => (
+  is      => 'ro',
+  lazy    => 1,
+  default => sub { Text::CSV_XS->new; },
+);
+
+#-------------------------------------------------------------------------------
+
+# callback to render a data structure as CSV
+sub _render_csv {
+  my ( $data, $controller, $c ) = @_;
+
+  my @keys;
+
+  # which fields should be returned in the CSV?
+  if ( $controller->returned_columns ) {
+    # use the fields that the controller says it will return
+    @keys = @{ $controller->returned_columns };
+  }
+  else {
+    # use all fields in the dataset, and return them in sort order
+    @keys = sort keys %{ $data->[0] };
+  }
+
+  # build a header row
+  my $status = $controller->_csv->combine(@keys);
+  die 'problem generating CSV header' unless $status;
+
+  my @output = ( $controller->_csv->string );
+
+  # it's a bit ugly, but we just walk the array and dump each row in turn
+  foreach my $row ( @$data ) {
+    my @values = ();
+    foreach my $key ( @keys ) {
+      push @values, $row->{$key};
+    }
+    $status = $controller->_csv->combine(@values);
+    die 'problem generating CSV' unless $status;
+
+    push @output, $controller->_csv->string;
+  }
+
+  return join "\n", @output;
+}
 
 #-------------------------------------------------------------------------------
 
