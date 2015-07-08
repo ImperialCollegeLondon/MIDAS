e67b8fb3303e88c3435099685a3f7603c0dff1f9
John Tate <jt6@sanger.ac.uk>
2015-07-08 17:09:19 +0100

    Add first proper front-end tests
    Added a module to take care of logging in. It's a bit convoluted, in
    order to work around the vagaries of the scoping of CasperJS in the
    mocha tests. Anyway, there are now some tests for (some of) the
    javascript in the summary page.

---
+++ test/config.js
@@ -0,0 +1,8 @@
+
+module.exports = {
+  user: {
+    username: "testuser",
+    password: "password"
+  }
+};
+
--- test/login.js
+++ test/login.js
@@ -1,78 +1,46 @@
 
-casper.test.begin("login functionality tests", 15, function(test) {
+var config = require("./config.js");
 
-  casper.start("http://localhost:8000/");
+module.exports = function(testSuite) {
 
-  casper.then(function() {
-    test.assertTitleMatch(/MIDAS/, "title matches");
-    test.assertExists("#signInButton", "found sign-in button");
-    test.assertDoesntExist("#signOutButton", "no sign-out button");
-  });
+  describe("Sign in", function() {
 
-  casper.then(function() {
-    test.comment("click sign-in button");
-    this.click("#signInButton");
-  });
-
-  casper.then(function() {
-    test.assertExists("h2", "Found page title");
-    test.assertSelectorHasText("h2", "Sign in", "clicking sign-in button takes us to the login page");
-  });
-
-  casper.then(function() {
-    test.comment("add username but no password");
-    this.fill("form[name='signin-form']", { username: "testuser" } );
-    this.click("button[type='submit']");
-  });
-
-  casper.then(function() {
-    test.assertSelectorHasText("h2", "Sign in", "still on sign-in page");
-    test.assertTextExists("Wrong username or password", "got expected error message");
-  });
-
-  casper.then(function() {
-    test.comment("add username but bad password");
-    this.fill("form[name='signin-form']", {
-      username: "testuser",
-      password: "badpassword"
+    before( function() {
+      casper.start("http://localhost:3001")
+            .thenOpen("http://localhost:3001/logout");
     });
-    this.click("button[type='submit']");
-  });
 
-  casper.then(function() {
-    test.assertSelectorHasText("h2", "Sign in", "still on sign-in page");
-    test.assertTextExists("Wrong username or password", "got expected error message");
-  });
-
-  casper.then(function() {
-    test.comment("add valid username and password");
-    this.fill("form[name='signin-form']", {
-      username: "testuser",
-      password: "password"
+    // make sure the user is signed out first
+    it("should start logged out", function() {
+      "#signInButton".should.be.inDOM;
     });
-    this.click("button[type='submit']");
+
+    it("should be on sign in page", function() {
+      casper.thenOpen("http://localhost:3001/login", function() {
+        "MIDAS".should.matchTitle;
+        "Sign in".should.be.textInDOM;
+      });
+    });
+
+    it("should be able to sign in", function() {
+      casper.then( function() {
+        "form[name='signin-form']".should.be.inDOM.and.be.visible;
+        this.fill(
+          "form[name='signin-form']",
+          { username: config.user.username,
+            password: config.user.password },
+          true
+        );
+      });
+    });
+
+    it("should be able to run tests requiring logged-in user", function() {
+      casper.then(function() {
+        testSuite.call(casper);
+      });
+    });
+
   });
 
-  casper.then(function() {
-    test.assertTextExists("Working to provide tools for the rapid identification", "redirected to home page");
-    test.assertDoesntExist("#signInButton", "no sign-in button");
-    test.assertExists("#signOutButton", "found sign-out button");
-  });
-
-  casper.then(function() {
-    test.comment("sign out");
-    this.click("#signOutButton");
-  });
-
-  casper.then(function() {
-    test.assertTextExists("Working to provide tools for the rapid identification", "redirected to home page");
-    test.assertExists("#signInButton", "found sign-in button");
-    test.assertDoesntExist("#signOutButton", "no sign-out button");
-  });
-
-  casper.run(function() {
-    test.done();
-  });
-
-});
+};
 
