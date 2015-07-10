30cd88fd00f29161e324b70a593f8b04cfd893b7
John Tate <jt6@sanger.ac.uk>
2015-07-10 16:16:20 +0100

    Accept env variables, use DB with multiple samples

--- app/run_test_server.sh
+++ app/run_test_server.sh
@@ -3,11 +3,12 @@
 export PERL5LIB=../../../perl5/lib/perl5:../../Bio-Metadata-Validator/lib:../../Bio-HICF-Schema/lib
 export CATALYST_CONFIG=midas.conf
 export CATALYST_CONFIG_LOCAL_SUFFIX=testing
-export CATALYST_DEBUG=1
-export DBIC_TRACE=1
+export CATALYST_DEBUG=$CATALYST_DEBUG || 1
+export DBIC_TRACE=$DBIC_TRACE || 1
 
-# sqlite3 testing.db < t/data/create_full_test_db.sql
-cp t/data/data.db temp_data.db
+# this is a SQLite DB that has multiple rows in the sample table, specifically for use
+# with the front-end tests
+cp t/data/multiple_samples.db temp_data.db
 cp t/data/user.db temp_user.db
 
 perl script/midas_server.pl --port 3001
