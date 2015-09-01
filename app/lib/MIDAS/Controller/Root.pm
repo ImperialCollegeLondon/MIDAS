e2bf1cde73f29a0c8b1ba0acf433e455a5a27730
John Tate <jt6@sanger.ac.uk>
2015-09-01 14:06:23 +0100

    Add action to generate list of antimicrobial names
    Add an action to the root controller to generate and return a list of
    the antimicrobial compound names from the database. Also add a link to
    the list on the data summary page.

--- app/lib/MIDAS/Controller/Root.pm
+++ app/lib/MIDAS/Controller/Root.pm
@@ -97,6 +97,64 @@ sub api : Local Args(0) {
 }
 
 #-------------------------------------------------------------------------------
+
+=head2 antimicrobials
+
+Returns the list of currently registered antimicrobial compound names as a
+plain text file. Since this list could include sensitive information, this
+action requires authentication.
+
+Unless the C<NO_CACHE> environment variable is set to true, the list will be
+cached when first generated and returned from cache subsequently.
+
+=cut
+
+sub antimicrobials : Local
+                     Args(0)
+                     Does('~NeedsAuth') {
+  my ( $self, $c ) = @_;
+
+  my $cache_key = 'antimicrobial_names';
+
+  # take note of the NO_CACHE env variable. If set, don't try to read from or
+  # push to the cache
+  my $names = $ENV{NO_CACHE}
+            ? ''
+            : $c->cache->get($cache_key);
+
+  if ( $names ) {
+    $c->log->debug( 'Root::antimicrobials: retrieved list from cache' )
+      if $c->debug;
+  }
+  else {
+    $c->log->debug( 'Root::antimicrobials: failed to retrieve list from cache; generating' )
+      if $c->debug;
+
+    my @names = $c->model('HICFDB::Antimicrobial')->search(
+      {},
+      {
+        order_by => { -asc => 'name' },
+        columns  => [ 'name' ],
+      }
+    );
+
+    my $now = DateTime->now;
+    $names = <<EOF_header;
+# antimicrobial compound names accepted by MIDAS
+# generated $now
+EOF_header
+
+    $names .= $_->name . "\n" foreach @names;
+
+    $c->cache->set($cache_key, $names) unless $ENV{NO_CACHE};
+  }
+
+  $c->res->content_type('text/plain');
+  $c->res->header('Content-disposition', qq(attachment; filename="MIDAS_antimicrobials.txt"));
+  $c->res->body($names);
+}
+
+#-------------------------------------------------------------------------------
 #- boilerplate actions ---------------------------------------------------------
 #-------------------------------------------------------------------------------
 
